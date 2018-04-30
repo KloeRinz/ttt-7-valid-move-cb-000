e2cd92d9fc276f553103297cdb37653a15c614aa
Carlos Miguel Pimentel <eleanorisyuyu@gmail.com>
2018-04-30 19:28:25 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,6 +3,8 @@ def valid_move?(board, index)
     if
       index >= 0 || index < 9
       valid_position = true
+    else
+      valid_position = false
     end
     def position_taken?(board, index)
       if
